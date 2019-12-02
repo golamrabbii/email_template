86feb606b43e7b17b749fba669247dc04c1d79bf
golamrabbii <rabbi29157@gmail.com>
2019-12-02 10:58:24 +0600

    API Upgrade mail

---
+++ api_upgrade_mail.py
@@ -0,0 +1,145 @@
+import mysql.connector
+import smtplib
+from smtplib import SMTP       
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from jinja2 import Environment       
+
+def send_mail(name,TO,auto_complete_count,geo_code_count,reverse_geo_code_count,distance_count,nearby_count):
+
+    TEMPLATE = """
+   <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Barikoi API Usage</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<style>
+    header img {
+        margin-top: 5px;
+        max-width: 200px;
+    }
+table {
+position: relative;
+font-family: 'Open Sans', sans-serif;
+border-collapse: collapse;
+margin-left: 45px;
+width: 727px;
+}
+td, th {
+border: 1px solid #dddddd;
+text-align: left;
+padding: 8px;
+}
+th{
+padding-top: 12px;
+padding-bottom: 12px;
+text-align: left;
+background-color: #4CAF50;
+color: white;
+}
+tr:nth-child(even) {
+background-color: #dddddd;
+}
+p{
+margin-left: 45px;
+}
+body{
+  position: relative;
+  margin-left: 250px;
+  margin-right: 250px;
+  border-style: 5px solid;
+  border-color: white;
+  box-shadow:5px 6px 100px 100px whitesmoke;
+  border-radius: 5px;
+  background-color: white;
+  font-family:'Open Sans',sans-serif;
+}
+</style>
+</head>
+<body>
+<header>
+<center>
+   <a href="https://barikoi.com"><img src="https://barikoi.com/views/assets/img/logo2.png" ></a>
+</center>
+</header>
+<br><br>
+<p style="font-size:15px;">Hi <b>{{name}}</b></p>
+<p style="font-size:15px;">Greetings from <b>Barikoi Technologies Limited</b></p>
+<p style="font-size:15px;">Thanking you for using Barikoi API. Here is your API usage for last week. (Last 7 Days)</p>
+          <br>
+          <table>
+            <thead>
+              <tr>
+                  <th>Autocomplete</th>
+                  <th>Geocode</th>
+                  <th>Reverse Geocode</th>
+                  <th>Nearby</th>
+                  <th>Distance</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{{autocomplete}}</td>
+                <td>{{geocode}}</td>
+                <td>{{reverse_geo}}</td>
+                <td>{{nearby}}</td>
+                <td>{{distance}}</td>
+              </tr>
+            </tbody>
+          </table>
+          <br>
+          <p style="font-size:14px;">Best Wishes</p> 
+          <p style="font-size:15px;"><b>Team Barikoi</b></p>
+<center>
+      <br>
+        <b> &copy; 2019 Barikoi Technologies Limited </b>
+</center>
+</body>
+</html> 
+    """  
+    msg = MIMEText(
+    Environment().from_string(TEMPLATE).render(
+       name = name,autocomplete = auto_complete_count,geocode = geo_code_count, reverse_geo = reverse_geo_code_count,nearby = nearby_count, distance = distance_count
+    ), "html"
+    )
+    FROM = "rh622984@gmail.com"
+    msg['Subject'] = "Barikoi API Usages"
+    msg['From'] = FROM
+    msg['To'] = TO
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    password = "01687922852"
+    server.starttls()
+    server.login(FROM,password)
+    server.sendmail(FROM, [TO], msg.as_string().encode('utf8'))
+    server.quit()
+
+if __name__ == "__main__":
+    
+    mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="12345",
+            database="test_db_",
+        )
+    query = "select user_id,autocomplete_count,geo_code_count,reverse_geo_code_count,distance_count,nearby_count from tokens where isActive=1 and user_id=1486"
+    cursor = mydb.cursor()
+    cursor.execute(query)
+    records = cursor.fetchall()
+    for i in records:
+        auto_complete_count = i[1]
+        geo_code_count = i[2]
+        reverse_geo_code_count =  i[3]
+        distance_count = i[4]
+        nearby_count = i[5]
+        query = "select name,email from users where id="+str(i[0])
+        cursor = mydb.cursor()
+        cursor.execute(query)
+        record = cursor.fetchall()
+        for j in record:
+            name = j[0]
+            email = j[1]
+        send_mail(name,email,auto_complete_count,geo_code_count,reverse_geo_code_count,distance_count,nearby_count)
